8ce3fe1e8757a700a9de1d7e4c299654ed201223
Alexander Devaykin <brz-da@ipt.fraunhofer.de>
2017-04-10 22:38:43 +0200

    Encapsulated spectrogram into a class

--- spectro.js
+++ spectro.js
@@ -1,32 +1,35 @@
-var gl;
-var canvas;
-var squareVerticesBuffer;
-var squareUVBuffer;
-var squareIdxBuffer;
-var mvMatrix;
-var shaderProgram;
-var vertexPositionAttribute;
-var vertexUVAttribute;
-var orthoMatrix;
+Spectrow = function(canvas_id) {
+this.canvas = document.getElementById(canvas_id);
 
-var cubeTexture;
-var cubeImage;
+this.gl;
+this.canvas;
+this.squareVerticesBuffer;
+this.squareUVBuffer;
+this.squareIdxBuffer;
+this.mvMatrix;
+this.shaderProgram;
+this.vertexPositionAttribute;
+this.vertexUVAttribute;
+this.orthoMatrix;
 
-function initWebGL(canv) {
-  gl = null;
+this.cubeTexture;
+this.cubeImage;
+
+this.initWebGL = function initWebGL(canv) {
+  this.gl = null;
 
   // Try to grab the standard context. If it fails, fallback to experimental.
-  gl = canv.getContext('webgl') || canv.getContext('experimental-webgl');
+  this.gl = canv.getContext('webgl') || canv.getContext('experimental-webgl');
 
   // If we don't have a GL context, give up now
-  if (!gl) {
+  if (!this.gl) {
     alert('Unable to initialize WebGL. Your browser may not support it.');
   }
 
-  return gl;
+  return this.gl;
 }
 
-function getShader(gl, id, type) {
+this.getShader = function getShader(gl, id, type) {
   var shaderScript, theSource, currentChild, shader;
 
   shaderScript = document.getElementById(id);
@@ -39,58 +42,58 @@ function getShader(gl, id, type) {
 
   if (!type) {
     if (shaderScript.type == 'x-shader/x-fragment') {
-      type = gl.FRAGMENT_SHADER;
+      type = this.gl.FRAGMENT_SHADER;
     } else if (shaderScript.type == 'x-shader/x-vertex') {
-      type = gl.VERTEX_SHADER;
+      type = this.gl.VERTEX_SHADER;
     } else {
       // Unknown shader type
       return null;
     }
   }
-  shader = gl.createShader(type);
+  shader = this.gl.createShader(type);
 
-  gl.shaderSource(shader, theSource);
+  this.gl.shaderSource(shader, theSource);
 
   // Compile the shader program
-  gl.compileShader(shader);
+  this.gl.compileShader(shader);
 
   // See if it compiled successfully
-  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-      console.log('An error occurred compiling the shaders: ' + gl.getShaderInfoLog(shader));
-      gl.deleteShader(shader);
+  if (!this.gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+      console.log('An error occurred compiling the shaders: ' + this.gl.getShaderInfoLog(shader));
+      this.gl.deleteShader(shader);
       return null;
   }
 
   return shader;
 }
 
-function initShaders() {
-  var fragmentShader = getShader(gl, 'shader-fs');
-  var vertexShader = getShader(gl, 'shader-vs');
+this.initShaders = function initShaders() {
+  var fragmentShader = this.getShader(this.gl, 'shader-fs');
+  var vertexShader = this.getShader(this.gl, 'shader-vs');
 
   // Create the shader program
 
-  shaderProgram = gl.createProgram();
-  gl.attachShader(shaderProgram, vertexShader);
-  gl.attachShader(shaderProgram, fragmentShader);
-  gl.linkProgram(shaderProgram);
+  this.shaderProgram = this.gl.createProgram();
+  this.gl.attachShader(this.shaderProgram, vertexShader);
+  this.gl.attachShader(this.shaderProgram, fragmentShader);
+  this.gl.linkProgram(this.shaderProgram);
 
   // If creating the shader program failed, alert
 
-  if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
-    console.log('Unable to initialize the shader program: ' + gl.getProgramInfoLog(shaderProgram));
+  if (!this.gl.getProgramParameter(this.shaderProgram, this.gl.LINK_STATUS)) {
+    console.log('Unable to initialize the shader program: ' + this.gl.getProgramInfoLog(this.shaderProgram));
   }
 
-  gl.useProgram(shaderProgram);
+  this.gl.useProgram(this.shaderProgram);
 
-  vertexPositionAttribute = gl.getAttribLocation(shaderProgram, 'aVertexPosition');
-  gl.enableVertexAttribArray(vertexPositionAttribute);
+  this.vertexPositionAttribute = this.gl.getAttribLocation(this.shaderProgram, 'aVertexPosition');
+  this.gl.enableVertexAttribArray(this.vertexPositionAttribute);
 
-  vertexUVAttribute = gl.getAttribLocation(shaderProgram, 'aUVPositions');
-  gl.enableVertexAttribArray(vertexUVAttribute);
+  this.vertexUVAttribute = this.gl.getAttribLocation(this.shaderProgram, 'aUVPositions');
+  this.gl.enableVertexAttribArray(this.vertexUVAttribute);
 }
 
-function initBuffers() {
+this.initBuffers = function initBuffers() {
   var vertices = [
     -1.0,  1.0, 0.0,
     -1.0, -1.0, 0.0,
@@ -110,124 +113,124 @@ function initBuffers() {
     2, 1, 3
   ]
 
-  squareVerticesBuffer = gl.createBuffer();
-  squareUVBuffer = gl.createBuffer();
-  squareIdxBuffer = gl.createBuffer();
+  this.squareVerticesBuffer = this.gl.createBuffer();
+  this.squareUVBuffer = this.gl.createBuffer();
+  this.squareIdxBuffer = this.gl.createBuffer();
 
-  gl.bindBuffer(gl.ARRAY_BUFFER, squareVerticesBuffer);
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+  this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.squareVerticesBuffer);
+  this.gl.bufferData(this.gl.ARRAY_BUFFER, new Float32Array(vertices), this.gl.STATIC_DRAW);
 
-  gl.bindBuffer(gl.ARRAY_BUFFER, squareUVBuffer);
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(uv), gl.STATIC_DRAW);
+  this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.squareUVBuffer);
+  this.gl.bufferData(this.gl.ARRAY_BUFFER, new Float32Array(uv), this.gl.STATIC_DRAW);
 
-  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, squareIdxBuffer);
-  gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
+  this.gl.bindBuffer(this.gl.ELEMENT_ARRAY_BUFFER, this.squareIdxBuffer);
+  this.gl.bufferData(this.gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), this.gl.STATIC_DRAW);
 }
 
-function loadIdentity() {
-  mvMatrix = Matrix.I(4);
+this.loadIdentity = function loadIdentity() {
+  this.mvMatrix = Matrix.I(4);
 }
 
-function multMatrix(m) {
-  mvMatrix = mvMatrix.x(m);
+this.multMatrix = function multMatrix(m) {
+  this.mvMatrix = this.mvMatrix.x(m);
 }
 
-function mvTranslate(v) {
-  multMatrix(Matrix.Translation($V([v[0], v[1], v[2]])).ensure4x4());
+this.mvTranslate = function mvTranslate(v) {
+  this.multMatrix(Matrix.Translation($V([v[0], v[1], v[2]])).ensure4x4());
 }
 
-function setAttributes() {
-  gl.bindBuffer(gl.ARRAY_BUFFER, squareVerticesBuffer);
-  gl.vertexAttribPointer(vertexPositionAttribute, 3, gl.FLOAT, false, 0, 0);
-  gl.bindBuffer(gl.ARRAY_BUFFER, squareUVBuffer);
-  gl.vertexAttribPointer(vertexUVAttribute, 2, gl.FLOAT, false, 0, 0);
+this.setAttributes = function setAttributes() {
+  this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.squareVerticesBuffer);
+  this.gl.vertexAttribPointer(this.vertexPositionAttribute, 3, this.gl.FLOAT, false, 0, 0);
+  this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.squareUVBuffer);
+  this.gl.vertexAttribPointer(this.vertexUVAttribute, 2, this.gl.FLOAT, false, 0, 0);
 }
 
-function setUniforms() {
-  var pUniform = gl.getUniformLocation(shaderProgram, "uPMatrix");
-  gl.uniformMatrix4fv(pUniform, false, new Float32Array(orthoMatrix.flatten()));
+this.setUniforms = function setUniforms() {
+  var pUniform = this.gl.getUniformLocation(this.shaderProgram, "uPMatrix");
+  this.gl.uniformMatrix4fv(pUniform, false, new Float32Array(this.orthoMatrix.flatten()));
 
-  var mvUniform = gl.getUniformLocation(shaderProgram, "uMVMatrix");
-  gl.uniformMatrix4fv(mvUniform, false, new Float32Array(mvMatrix.flatten()));
+  var mvUniform = this.gl.getUniformLocation(this.shaderProgram, "uMVMatrix");
+  this.gl.uniformMatrix4fv(mvUniform, false, new Float32Array(this.mvMatrix.flatten()));
 
-  var sceen_width_uniform = gl.getUniformLocation(shaderProgram, "screen_width");
-  var sceen_height_uniform = gl.getUniformLocation(shaderProgram, "screen_height");
-  gl.uniform1i(sceen_width_uniform, canvas.width);
-  gl.uniform1i(sceen_height_uniform, canvas.height);
+  var sceen_width_uniform = this.gl.getUniformLocation(this.shaderProgram, "screen_width");
+  var sceen_height_uniform = this.gl.getUniformLocation(this.shaderProgram, "screen_height");
+  this.gl.uniform1i(sceen_width_uniform, this.canvas.width);
+  this.gl.uniform1i(sceen_height_uniform, this.canvas.height);
 
-  gl.activeTexture(gl.TEXTURE0);
-  gl.bindTexture(gl.TEXTURE_2D, cubeTexture);
-  gl.uniform1i(gl.getUniformLocation(shaderProgram, 'uSampler'), 0);
+  this.gl.activeTexture(this.gl.TEXTURE0);
+  this.gl.bindTexture(this.gl.TEXTURE_2D, this.cubeTexture);
+  this.gl.uniform1i(this.gl.getUniformLocation(this.shaderProgram, 'uSampler'), 0);
 }
 
-function drawScene() {
-  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+this.drawScene = function drawScene(inst) {
+  inst.gl.clear(inst.gl.COLOR_BUFFER_BIT | inst.gl.DEPTH_BUFFER_BIT);
 
-  orthoMatrix = makeOrtho(-1.0, 1.0, -1.0, 1.0, 0.1, 100.0);
+  inst.orthoMatrix = makeOrtho(-1.0, 1.0, -1.0, 1.0, 0.1, 100.0);
 
-  loadIdentity();
-  mvTranslate([0.0, 0.0, -0.2]);
+  inst.loadIdentity();
+  inst.mvTranslate([0.0, 0.0, -0.2]);
 
-  setAttributes();
-  setUniforms();
+  inst.setAttributes();
+  inst.setUniforms();
 
-  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, squareIdxBuffer);
-  gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+  inst.gl.bindBuffer(inst.gl.ELEMENT_ARRAY_BUFFER, inst.squareIdxBuffer);
+  inst.gl.drawArrays(inst.gl.TRIANGLE_STRIP, 0, 4);
 }
 
-function handleTextureLoaded(image, texture) {
-  gl.bindTexture(gl.TEXTURE_2D, texture);
-  gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, 64, 64, 0, gl.RGB, gl.UNSIGNED_BYTE, new Uint8Array(image));
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_NEAREST);
-  gl.generateMipmap(gl.TEXTURE_2D);
-  gl.bindTexture(gl.TEXTURE_2D, null);
+this.handleTextureLoaded = function handleTextureLoaded(image, texture) {
+  this.gl.bindTexture(this.gl.TEXTURE_2D, texture);
+  this.gl.texImage2D(this.gl.TEXTURE_2D, 0, this.gl.RGB, 64, 64, 0, this.gl.RGB, this.gl.UNSIGNED_BYTE, new Uint8Array(image));
+  this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MAG_FILTER, this.gl.LINEAR);
+  this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MIN_FILTER, this.gl.LINEAR_MIPMAP_NEAREST);
+  this.gl.generateMipmap(this.gl.TEXTURE_2D);
+  this.gl.bindTexture(this.gl.TEXTURE_2D, null);
 }
 
-function initTextures() {
-  cubeTexture = gl.createTexture();
+this.initTextures = function initTextures() {
+  this.cubeTexture = this.gl.createTexture();
 
-  cubeImage = Array.apply(null, Array(64*64*3)).map(Number.prototype.valueOf, 128);
+  this.cubeImage = Array.apply(null, Array(64*64*3)).map(Number.prototype.valueOf, 128);
   for(i=0; i<64*3; i+=3) {
-    cubeImage[i] = 255;
+    this.cubeImage[i] = 255;
   }
 
   for(i=1; i<64*3; i+=3) {
-    cubeImage[i+64*3] = 255;
+    this.cubeImage[i+64*3] = 255;
   }
 
   for(i=2; i<64*3; i+=3) {
-    cubeImage[i+64*3*2] = 255;
+    this.cubeImage[i+64*3*2] = 255;
   }
 
-  handleTextureLoaded(cubeImage, cubeTexture);
+  this.handleTextureLoaded(this.cubeImage, this.cubeTexture);
 }
 
-function start() {
-  canvas = document.getElementById('glCanvas');
-
+this.start = function start() {
   // Initialize the GL context
-  gl = initWebGL(canvas);
+  this.gl = this.initWebGL(this.canvas);
 
   // Only continue if WebGL is available and working
-  if (!gl) {
+  if (!this.gl) {
     return;
   }
 
-  gl.viewport(0, 0, canvas.width, canvas.height);
+  this.gl.viewport(0, 0, this.canvas.width, this.canvas.height);
 
   // Set clear color to black, fully opaque
-  gl.clearColor(0.0, 0.4, 0.0, 1.0);
+  this.gl.clearColor(0.0, 0.4, 0.0, 1.0);
   // Enable depth testing
-  gl.enable(gl.DEPTH_TEST);
+  this.gl.enable(this.gl.DEPTH_TEST);
   // Near things obscure far things
-  gl.depthFunc(gl.LEQUAL);
+  this.gl.depthFunc(this.gl.LEQUAL);
   // Clear the color as well as the depth buffer.
-  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+  this.gl.clear(this.gl.COLOR_BUFFER_BIT | this.gl.DEPTH_BUFFER_BIT);
 
-  initShaders();
-  initBuffers();
-  initTextures();
+  this.initShaders();
+  this.initBuffers();
+  this.initTextures();
 
-  setInterval(drawScene, 500);
+  setInterval(this.drawScene(this), 500);
+}
+
 }
